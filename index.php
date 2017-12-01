7e123a22bce54b3b252bef93b1adddd74b00f638
Eric Lang <eric@Erics-MacBook-Pro.local>
2017-12-01 10:05:45 -0600

    redesign and restructuring

--- index.php
+++ index.php
@@ -11,35 +11,36 @@
 
 <div id="mainwrapper">
   <!--hero-->
-  <div class="hero">
-    <div class="image-bg">
-      <div class="main-cta-wrap">
-        <div class="main-cta">
-          <h1 class="main-mark">Field Day</h1>
-          <p class="corrected">Build innovative apps & games to help people explore your work</p>
-        </div>
-        <a class="drawer-toggle drawerleft-toggle"><span class="drawer-button"><img src="assets/img/ui/arrow.svg"><p>Make</p></span></a>
-        <a class="drawer-toggle drawertop-toggle"><span class="drawer-button"><img src="assets/img/ui/arrow.svg"><p>Play</p></span></a>
-        <a class="drawer-toggle drawerright-toggle"><span class="drawer-button"><img src="assets/img/ui/arrow.svg"><p>Learn</p></span></a>
-        <div class="scrollindicator">
-          <a href="#our_process" class="icon-scroll"></a>
-        </div>
+  <div id="carousel" class="hero carousel slide carousel-fade" data-ride="carousel">
+      <ol class="carousel-indicators">
+          <li data-target="#carousel" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel" data-slide-to="1"></li>
+          <li data-target="#carousel" data-slide-to="2"></li>
+      </ol>
+      <!-- Carousel items -->
+      <div class="carousel-inner">
+          <div class="active item"></div>
+          <div class="item"></div>
+          <div class="item"></div>
       </div>
-    </div>
+      <!-- Carousel nav -->
   </div>
+
+
+  <!--<a class="drawer-toggle drawerleft-toggle"><span class="drawer-button"><img src="assets/img/ui/arrow.svg"><p>Make</p></span></a>
+  <a class="drawer-toggle drawertop-toggle"><span class="drawer-button"><img src="assets/img/ui/arrow.svg"><p>Play</p></span></a>
+  <a class="drawer-toggle drawerright-toggle"><span class="drawer-button"><img src="assets/img/ui/arrow.svg"><p>Learn</p></span></a>-->
+
   <section id="our_process" class="our_process">
     <div class="processbg one"></div>
-    <div class="processbg two"></div>
-    <div class="processbg three"></div>
-    <div class="processbg four"></div>
     <div class="process intro">
       <span class="veryslowreveal spacedust one"></span>
       <span class="veryslowreveal spacedust two"></span>
       <span class="offscreen test one"></span>
       <div class="intro-content">
         <img class="bg1trigger reveal float" src="assets/img/illustrations/astronaut1.png" class="astronaut">
-        <p class="corrected plainreveal big light">"Inasmuch as we can capture that wonder, that pure excitement about the complexity and beauty of the world—that will be how much we actually inspire kids with a love for learning." </p>
-        <p class="plainreveal big">- David Gagnon</p>
+        <h2 class="light">We are Field Day</h2>
+        <p class="corrected plainreveal big light">"What if kids had the same curiosity and sense of discovery that drives the best researchers at universities? We make games and apps to help foster that wonder. Based at the Wisconsin Center for Educational Research at the University of Wisconsin - Madison, we are an interdisciplinary research lab of software engineers, artists, educational researchers and storytellers exploring the intersection of current learning science and media design, specializing in mobile media, video games, and simulation. " </p>
       </div>
       <div class="starfield topright">
         <span class="star star1"></span>
@@ -73,185 +74,6 @@
           <a href="#process1" class="icon-scroll"></a>
         </div>
     </div>
-
-    <div id="process1" class="process process1">
-      <div class="starfield topleft">
-        <span class="star star1"></span>
-        <span class="star star2"></span>
-        <span class="star star3"></span>
-        <span class="star star4"></span>
-        <span class="star star5"></span>
-      </div>
-      <span class="veryslowreveal spacedust one"></span>
-      <span class="veryslowreveal spacedust two"></span>
-      <div class="row process_content">
-        <div class="illustrations col-sm-6 col-sm-push-6">
-            <img class="reveal spaceplane" src="assets/img/illustrations/spaceplane-holder.png">
-            <span class="blueprint-icon hideforsmall one"></span>
-            <span class="blueprint-icon hideforsmall two"></span>
-            <span class="blueprint-icon hideforsmall three"></span>
-            <span class="blueprint-icon hideforsmall four"></span>
-            <span class="blueprint-icon hideforsmall five"></span>
-        </div>
-        <div class="info col-sm-6 col-sm-pull-6 plainreveal">
-          <div class="infowrap">
-            <h2 class="bg2trigger corrected light">You need a team of designers & engineers to make educational media</h2>
-            <p>Researchers often have an idea for an app or game that will let the public connect with the research. So . . . now what? Getting research out there is exciting, but it’s also a lot of work. You don’t have the resources and infrastructure to build the kind of digital media that will reach the public.
-
-</p>
-            <a href="#polished2" class="button white">Complexity doesn't scare us.</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div id="polished2" class="process process2">
-      <span class="veryslowreveal spacedust one"></span>
-      <span class="veryslowreveal spacedust two"></span>
-      <div class="row process_content">
-        <div class="illustrations plainreveal col-sm-6 col-sm-push-6">
-            <img class="spaceplane" src="assets/img/illustrations/spaceplane-holder.png">
-            <div class="exhaustwrap">
-              <img class="exhaust one" src="assets/img/illustrations/exhaust.png">
-              <img class="exhaust two" src="assets/img/illustrations/exhaust2.png">
-            </div>
-            <div class="exhaustwrap two">
-              <img class="exhaust one late" src="assets/img/illustrations/exhaust.png">
-              <img class="exhaust two late" src="assets/img/illustrations/exhaust2.png">
-            </div>
-        </div>
-        <div class="info col-sm-6 col-sm-pull-6 plainreveal">
-          <div class="infowrap">
-            <h2 class="bg3trigger corrected">The result of working without a design team is wasted time and money</h2>
-            <p class="">You might be thinking designers slow things down and cost too much money. We get it―sometimes collaboration is just a code word for lots of meetings. That’s why we’ve developed a process that is super efficient, requires little of your time and impresses grant reviewers. This is how you can kill it with broader impacts.
-</p>
-            <a href="#polished3" class="button black">Partner with a team that gets it</a>
-          </div>
-        </div>
-      </div>
-      <div class="starfield topright">
-        <span class="star star1"></span>
-        <span class="star star2"></span>
-        <span class="star star3"></span>
-        <span class="star star4"></span>
-        <span class="star star5"></span>
-      </div>
-      <div class="starfield topleft">
-        <span class="star star1"></span>
-        <span class="star star2"></span>
-        <span class="star star3"></span>
-        <span class="star star4"></span>
-        <span class="star star5"></span>
-      </div>
-    </div>
-    <div id="polished3" class="process process3">
-      <div class="starfield topright">
-        <span class="star star1"></span>
-        <span class="star star2"></span>
-        <span class="star star3"></span>
-        <span class="star star4"></span>
-        <span class="star star5"></span>
-      </div>
-      <div class="starfield topleft">
-        <span class="star star1"></span>
-        <span class="star star2"></span>
-        <span class="star star3"></span>
-        <span class="star star4"></span>
-        <span class="star star5"></span>
-      </div>
-      <div class="starfield bottomright">
-        <span class="star star1"></span>
-        <span class="star star2"></span>
-        <span class="star star3"></span>
-        <span class="star star4"></span>
-        <span class="star star5"></span>
-      </div>
-      <span class="veryslowreveal spacedust one"></span>
-      <span class="veryslowreveal spacedust two"></span>
-      <div class="row process_content">
-        <div class="illustrations col-sm-6 col-sm-push-6">
-            <img class="reveal spaceplane" src="assets/img/illustrations/spaceplane-holder.png">
-            <img class="skylabreveal skylab" src="assets/img/illustrations/skylab.png">
-          <img src="assets/img/illustrations/astronaut1.png" class="slowreveal float astronaut one">
-          <img src="assets/img/illustrations/astronaut2.png" class="slowreveal floatslow astronaut two">
-        </div>
-        <div class="info col-sm-6 col-sm-pull-6 plainreveal">
-          <div class="infowrap">
-            <h2 class="bg4trigger corrected">Let's make a game or app that helps the public understand your research</h2>
-            <p class="">We partner with the subject expert (you) and educators to design an app or game. Then we find large-scale distribution partnerships resulting in hundreds of thousands of plays. Finally, we research your app or game to learn more about how people learn.</p>
-            <button data-toggle="modal" data-target="#contactModal" class="button white">Work with Field Day</button>
-          </div>
-      </div>
-      </div>
-    </div>
-
-  </section>
-
-  <section class="mission">
-    <span class="bg-triangle"></span>
-    <div class="container pitch">
-      <h2 class="uppercase">Your Project in Three Phases</h2>
-    </div>
-    <div class="row container">
-      <div class="processreveal card-wrap col-sm-4">
-        <div class="card white">
-          <img class="card-thumb" src="assets/img/illustrations/process1.png">
-          <h3>Design & Grant Writing</h3>
-          <p>Our first step is to create a clear design plan.  We’ll also work with you to write grants and secure the funding needed to move forward. Starts around $10,000</p>
-          <img class="card-flag" src="assets/img/illustrations/cornerflag1.png">
-        </div>
-      </div>
-      <div class="processreveal card-wrap col-sm-4">
-        <div class="card white">
-          <img class="card-thumb" src="assets/img/illustrations/process2.png">
-          <h3>Production</h3>
-          <p>Hit go with our team of designers, software engineers, artists and education researchers. Starts around $30,000</p>
-          <img class="card-flag" src="assets/img/illustrations/cornerflag2.png">
-        </div>
-      </div>
-      <div class="processreveal card-wrap col-sm-4">
-        <div class="card white">
-          <img class="card-thumb" src="assets/img/illustrations/process3.png">
-          <h3>Distribution</h3>
-          <p>Time to spread the word. We identify partners to connect with our target audience.
-</p>
-          <img class="card-flag" src="assets/img/illustrations/cornerflag3.png">
-        </div>
-      </div>
-    </div>
-    <div id="maincta" class="maincta">
-      <img src="assets/img/illustrations/spaceplane-flight.png">
-      <h2 class="uppercase">Work with Us</h2>
-      <p>We work with scientists, researchers, teachers, artists, story tellers, and all-around rad people to make things together to impact our communities.  </p>
-      <button data-toggle="modal" data-target="#contactModal" class="button small white" >Work with Field Day</button>
-    </div>
-    <div class="hideforsmall starfield topright">
-        <span class="star black star1"></span>
-        <span class="star black star2"></span>
-        <span class="star black star3"></span>
-        <span class="star black star4"></span>
-        <span class="star black star5"></span>
-      </div>
-      <div class="hideforsmall starfield topleft">
-        <span class="star black star1"></span>
-        <span class="star black star2"></span>
-        <span class="star black star3"></span>
-        <span class="star black star4"></span>
-        <span class="star black star5"></span>
-      </div>
-      <div class="starfield bottomright">
-        <span class="star star1"></span>
-        <span class="star star2"></span>
-        <span class="star star3"></span>
-        <span class="star star4"></span>
-        <span class="star star5"></span>
-      </div>
-      <div class="starfield bottomleft">
-        <span class="star star1"></span>
-        <span class="star star2"></span>
-        <span class="star star3"></span>
-        <span class="star star4"></span>
-        <span class="star star5"></span>
-      </div>
   </section>
   <section class="blogs">
     <div class="row container large">
@@ -361,6 +183,9 @@
 
 <!--template js-->
 <script>
+
+//hero slider
+$('.carousel').carousel();
 
 // homepage scripts & scroll reveal elements & animations
 var fastreveal = {
