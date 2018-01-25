2bea665bb435686d9d9a414fdaab8e1a14bdd137
David Gagnon <mrdavidgagnon@gmail.com>
2018-01-24 21:18:28 -0600

    effin cached css in chrome

--- includes/header.php
+++ includes/header.php
@@ -23,4 +23,4 @@
   ga('send', 'pageview');
   ga('linker:autoLink', ['arisgames.org','fielddaylab.org','siftr.org'], false, true);
 </script>
-<link rel="stylesheet" href="/assets/css/styles.css">
+<link rel="stylesheet" href="/assets/css/styles.css?version=2">
