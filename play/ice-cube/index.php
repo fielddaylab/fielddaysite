d052776a517b2fa66195b09a622aefdec6bc22c2
David Gagnon <djgagnon@wisc.edu>
2018-04-27 12:43:14 -0400

    More sections on icecube

--- play/ice-cube/index.php
+++ play/ice-cube/index.php
@@ -56,8 +56,11 @@
     <div class="col-sm-4">
       <div class="about-nav follow-scroll">
         <ul>
-        <li><a href="#production">Production Team</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#goal">Goal</a></li>
+        <li><a href="#objectives">Learning Objectives</a></li>
         <li><a href="#museums">Museums</a></li>
+        <li><a href="#production">Production Team</a></li>
         <li><a href="#source">Source Code</a></li>
         <li><a href="#funding">Funding</a></li>
         </ul>
@@ -69,6 +72,19 @@
       <p>The IceCube VR experience puts you in the role of an astrophysicist who journeys deep into space to track down the source of a neutrino detected by the IceCube observatory in Antarctica. At the research station at the South Pole, you will see the large instrument, buried in the ice, detect a mesage from across the universe. Using your “impossibility drive,” you will follow the neutrinos path back to the black hole it was emitted from.
 Along the way, you will be able to take in awe inspiring views from the edge of the solar system, looking back from the milky way and dangerously close to a black hole. You will be able to see using not only your normal visible spectrum, but also using X-ray vision and neutrino vision, revealing entirely different views of space.
 </p>
+      <h2 id="goal">Goal, Audience and Context</h2>
+      <p>Explain the exciting, but hard to understand research taking place by the NSF funded IceCube project to middle-school and higher aged public audiences in museum contexts.</p>
+
+      <h2 id="objectives">Learning Objectives</h2>
+      <ul>
+        <li>IceCube is a research project in Antartica that studies the universe by detecting Neutrinos</li>
+        <li>Neutrinos are very interesting particles that usually pass right through matter and allow us to detect extreme objects like black holes.</li>
+      </ul>
+      
+      <h2 id="museums">Museums</h2>
+      <p>We are looking for additional museums who would like to put this VR experience on exhibit. Feel free to use it right now, no strings attached. Better yet, reach out by clicking "Work With Us" if you would like a consultation or would like to discuss customization.</p>
+
+
       <h2 id="production">Production Team</h2>
       <ul>
         <li>Kevin Ponto : Principal Investigator</li>
@@ -84,9 +100,6 @@ Along the way, you will be able to take in awe inspiring views from the edge of 
         <li>James Madsen : Subject Expert</li>
         <li>Rebecca Cors : Evaluation</li>
       </ul>
-      
-      <h2 id="museums">Museums</h2>
-      <p>We are looking for additional museums who would like to put this VR experience on exhibit. Feel free to use it right now, no strings attached. Better yet, reach out by clicking "Work With Us" if you would like a consultation or would like to discuss customization.</p>
       
       <h2 id="source">Source Code</h2>
       <p>This is a MIT Licensed, Opensource project. We would love to have some help in developing new potentials. Contribute to the code at <a href="https://github.com/fielddaylab/ICECUBEVR" target="_blank">https://github.com/fielddaylab/ICECUBEVR</a></p>
