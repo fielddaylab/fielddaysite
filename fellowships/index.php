8f0b09eb6be07fb8326d4292a7b31fc83a7a493f
Michael Tolly <miketolly@gmail.com>
2020-01-13 13:43:44 -0600

    Hide Aqualab fellowship; update Dashboard fellowship

--- fellowships/index.php
+++ fellowships/index.php
@@ -143,18 +143,19 @@
 
         <li class="gridder-list" data-griddercontent="#game15">
           <div class="game-thumbnail">
-            <img src="/assets/img/illustrations/mountainrange-dark.png">
+            <img src="/assets/img/fellowships/game-dashboard.jpg">
           </div>
           <div class="card-summary">
             <div class="info">
-              <h3>Open Game Data Fellowship</h3>
-              <h4 class="date">Summer 2020</h4>
-              <p>Dive into this exciting new opportunity! We are selecting a group of 3rd-5th grade teachers to brainstorm a new game about shipwrecks and underwater archaeology in the Great Lakes.</p>
+              <h3>Game Dashboard Fellowship</h3>
+              <h4 class="date">Spring 2020</h4>
+              <p>In this fellowship, you will join Jenn and our other designers to create a dashboard to monitor the progress of all your students.</p>
             </div>
             <span class="card-triangle"></span>
           </div>
         </li>
 
+        <!--
         <li class="gridder-list" data-griddercontent="#game14">
           <div class="game-thumbnail">
             <img src="/assets/img/fellowships/aqualab.jpg">
@@ -168,6 +169,7 @@
             <span class="card-triangle"></span>
           </div>
         </li>
+        -->
 
       <!-- Begin Past Fellowships-->
       <h2 class="section-title centered">Past Fellowships</h2>
@@ -360,21 +362,22 @@
 <!-- Fellowship Expanded content -->
 <div id="game15" class="gridder-content">
   <div class="row game-inner game15-inner">
-    <h3>Open Game Data Fellowship</h3>
-    <span>Deadline: January 10, 2020</span>
+    <h3>Game Dashboard Fellowship</h3>
+    <span>Deadline: February 10, 2020</span>
     <span>Stipend: $200 / teacher</span>
-    <span><a target="_blank" href="https://drive.google.com/open?id=1NWJ8eQy9rd5XYmP9Yl_cx2bgwv9TC3XiXfKsa_KmYCA">More Info</a></span>
+    <span><a target="_blank" href="https://docs.google.com/document/d/1hAIGTx-a0YG0m0tkhxP4RDMchMyWq8ic9IXJuC0nmA4/edit">More Info</a></span>
     <div class="wrapper">
       <div class="col-md-7 game-expanded">
-        <p>Dive into this exciting new opportunity! We are selecting a group of 3rd-5th grade teachers to brainstorm a new game about shipwrecks and underwater archaeology in the Great Lakes. You will join forces with Field Day, Wisconsin Sea Grant, the Wisconsin Historical Society, and Wisconsin Public Television. By combining your classroom expertise and our design experience we will create something amazing that will get used by teachers and students throughout the state!</p>
-        <a class="button small filled black" href="https://docs.google.com/forms/d/e/1FAIpQLSelgpUrxb5LLe_5rvFuhP6fhJUVIPYhB-qdZe7LIb73gayMgA/viewform?usp=sf_link" onclick="ga('send', 'event', 'Apply', 'Click')" target="_blank">Apply Now</a>
+        <p>When Jenn was a teacher, she wanted a way to track students’ progress while they were playing learning games. In this fellowship, you will join Jenn and our other designers to create a dashboard to monitor the progress of all your students. Blending the newest machine learning and assessment research with your teaching expertise, we will create a tool that supports teaching. This dashboard will be designed for use with Field Day's newest game, Lakeland, but will it serve as a model to be used across all our games.</p>
+        <a class="button small filled black" href="https://docs.google.com/forms/d/e/1FAIpQLScOW0_zHIbhtQldUcc7x6StN3JImY_wOE-Z8QTZDDLf9ncabw/viewform?usp=sf_link" onclick="ga('send', 'event', 'Apply', 'Click')" target="_blank">Apply Now</a>
       </div>
       <div class="col-md-5 cta">
-        <img class="expanded-thumb img-responsive" src="/assets/img/illustrations/mountainrange-dark.png">
+        <img class="expanded-thumb img-responsive" src="/assets/img/fellowships/game-dashboard.jpg">
       </div>
     </div>
   </div>
 </div>
+<!--
 <div id="game14" class="gridder-content">
   <div class="row game-inner game14-inner">
     <h3>Aqualab Fellowship</h3>
@@ -392,6 +395,7 @@
     </div>
   </div>
 </div>
+-->
 
 <div id="game13" class="gridder-content fellowship-finished">
   <div class="row game-inner game13-inner">
