7b3464e9d034e891b33e8aa4b6b3d87147965f6a
Eric Lang <eric@comp.local>
2020-03-26 17:04:36 -0500

    about page

--- about/index.php
+++ about/index.php
@@ -79,7 +79,7 @@
           </div>
         </li>
 
-        <li class="gridder-list" data-griddercontent="#profile1">
+        <li class="gridder-list" data-griddercontent="#profile2">
           <div class="profile-thumbnail">
             <img src="/assets/img/profiles/sarah.jpg">
             <div class="card-summary">
@@ -89,7 +89,7 @@
           </div>
         </li>
 
-        <li class="gridder-list" data-griddercontent="#profile1">
+        <li class="gridder-list" data-griddercontent="#profile3">
           <div class="profile-thumbnail">
             <img src="/assets/img/profiles/jim.jpg">
             <div class="card-summary">
@@ -99,7 +99,7 @@
           </div>
         </li>
 
-        <li class="gridder-list" data-griddercontent="#profile1">
+        <li class="gridder-list" data-griddercontent="#profile4">
           <div class="profile-thumbnail">
             <img src="/assets/img/profiles/eric.jpg">
             <div class="card-summary">
@@ -109,7 +109,7 @@
           </div>
         </li>
 
-        <li class="gridder-list" data-griddercontent="#profile1">
+        <li class="gridder-list" data-griddercontent="#profile5">
           <div class="profile-thumbnail">
             <img src="/assets/img/profiles/mike.jpg">
             <div class="card-summary">
@@ -119,17 +119,17 @@
           </div>
         </li>
 
-        <li class="gridder-list" data-griddercontent="#profile1">
+        <li class="gridder-list" data-griddercontent="#profile6">
           <div class="profile-thumbnail">
-            <img src="/assets/img/profiles/lindy.jpg">
+            <img src="/assets/img/profiles/autumn.jpg">
             <div class="card-summary">
-              <h3>Lindy Biller</h3>
+              <h3>Autumn Beauchesne</h3>
               <span class="card-triangle"></span>
             </div>
           </div>
         </li>
 
-        <li class="gridder-list" data-griddercontent="#profile1">
+        <li class="gridder-list" data-griddercontent="#profile7">
           <div class="profile-thumbnail">
             <img src="/assets/img/profiles/jenn.jpg">
             <div class="card-summary">
@@ -139,11 +139,11 @@
           </div>
         </li>
 
-        <li class="gridder-list" data-griddercontent="#profile1">
+        <li class="gridder-list" data-griddercontent="#profile8">
           <div class="profile-thumbnail">
-            <img src="/assets/img/profiles/autumn.jpg">
+            <img src="/assets/img/profiles/lindy.jpg">
             <div class="card-summary">
-              <h3>Autumn Beauchesne</h3>
+              <h3>Lindy Biller</h3>
               <span class="card-triangle"></span>
             </div>
           </div>
@@ -160,6 +160,123 @@
         <p class="small">David is a researcher at <a href="https://www.wcer.wisc.edu/" target="_bank">WCER</a> and a co-founder of the PLAY MAKE LEARN conference and the Games and Learning Summit. He is a PI on several research projects that explore video games and educational data mining, co-design, mixed reality (VR, AR), and location-based games. David created ARIS, which supported the development of thousands of location-based AR projects and <a href="https://scholar.google.com/scholar?start=0&q=arisgames.org&hl=en&as_sdt=0,50" target="_blank">hundreds of educational papers.</a> David’s work supports other research, from the <a href="https://fielddaylab.wisc.edu/opengamedata/" target="_blank">Open Game Data,</a> which shares data sets with educational researchers, to Field Day’s <a href="https://fielddaylab.wisc.edu/fellowships/" target="_blank">teacher fellowship program,</a> which serves as a site to study teacher professional growth.</p>
         <p class="small">David has been the producer or lead game designer for over a dozen award-winning educational games in topics such as engineering, science, mathematics, history, music, and astrophysics.  He has been an invited guest or keynote speaker for the White House Office of Science and Technology Policy, the Smithsonian National Museum of African American History and Culture, the MacArthur Foundation, Pearson Foundation, Serious Play, and various smaller conferences and workshops. </p>
         <p class="small">David knows how educational issues affect real kids, which allows him to stick to his values. He has big dreams for how video games can make an impact. David brings energy and optimism to every project, from research to game design, and his enthusiasm is contagious. </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="profile2" class="gridder-content">
+  <div class="row profile-inner profile-inner">
+    <h3>Sarah Gagnon</h3>
+    <span>Creative Director</span>
+    <div class="wrapper">
+      <div class="col-md-12 profile-expanded">
+        <p class="small">Sarah is an artist, UX designer, and messaging expert. She led the way in developing our vision for making games that are more than just research instruments. She cares about helping kids learn, and she knows they won’t learn anything if they don’t care. She pushes us to make games that are beautiful and compelling, not just games that work. 
+</p>
+        <p class="small">Sarah created our in-house art and communications team, knowing that art is key to bringing our games to the public. She leads game design projects and pitches concepts for new games. She also leads our communications team to ensure that all our games and projects have a web presence. Her growing interest in Media Studies and theory influenced by the psychoanalytic tradition is the foundation of her focus on learning games and their role in society. 
+</p>
+        <p class="small">As an artist, Sarah laughed when her daughter said she was “raised by scientists.” In reality, Sarah’s intuition as a non-scientist gives her crucial insight into what we make. She knows how to ask the right questions and empathize with the people who will end up playing our games.  
+</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="profile3" class="gridder-content">
+  <div class="row profile-inner profile-inner">
+    <h3>Jim Matthews</h3>
+    <span>Education Director and Associate Researcher </span>
+    <div class="wrapper">
+      <div class="col-md-12 profile-expanded">
+        <p class="small">Jim is a teacher, researcher, and designer whose work explores the intersection of place, design, and civic engagement. In particular, he’s interested in creating and researching new media-based experiences (e.g., geo-locative games, place-based stories, and field research tools) aimed at connecting youth and adults with people, places, and issues in their community. 
+</p>
+        <p class="small">As Education Director, Jim leads the development of our Teacher Fellowship program, a key to the successful implementation of our games into schools across the country. He’s interested in the creative ways teachers are already using games and what we can learn from their expertise. As a teacher himself, Jim is passionate about co-design. He cares about making games that actually work in the classroom, and he’s always looking for the best ways to bring teachers into the design process.
+</p>
+        <p class="small">In addition to his work at Field Day, Jim is a teacher at <a href="http://www.clarkstreetcommunityschool.com/" target="_blank">Clark Street Community School,</a> a place- and project-based public high school in Middleton, WI. 
+</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="profile4" class="gridder-content">
+  <div class="row profile-inner profile-inner">
+    <h3>Eric Lang</h3>
+    <span>Art Director and Lead Designer</span>
+    <div class="wrapper">
+      <div class="col-md-12 profile-expanded">
+        <p class="small">Eric Lang has a unique combination of skills encompassing the artistic and the technical. He carries a heavy load as Lead Designer all the UI and graphic design across all our games, mixed reality projects, web content, print material, and front end development. 
+</p>
+        <p class="small">As Art Director, Eric works with a wide selection of artists and brings their work into one cohesive project. He also functions as the lead artist on several projects, such as the Field Day brand and the Lakeland game. He’s incredibly talented at story development, world building, and general strategy and vision-casting for our team. 
+</p>
+        <p class="small">Eric cares about making games that are fun, compelling, and amazingly weird. His favorite era is the 1980’s, his favorite futuristic digital landscape painter is Simon Stålenhag, and his favorite bread is sourdough.
+</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="profile5" class="gridder-content">
+  <div class="row profile-inner profile-inner">
+    <h3>Mike Tolly</h3>
+    <span>Software Developer and Game Designer</span>
+    <div class="wrapper">
+      <div class="col-md-12 profile-expanded">
+        <p class="small">Mike Tolly is an awesome developer with a serious knack for game design. He worked as lead developer on STEMPorts, Play the Past, ARIS, The Firm, Viola, and Siftr, as well as helping on Jo Wilder and the Capitol Case. Mike has a wide breadth of programming ability, with experience in web design, web apps, game scripting, native mobile development, and systems administration.
+</p>
+        <p class="small">Mike’s ability to push through and get stuff done can feel a little bit magical. In reality, it’s a combination of skill and focus. (And, okay, maybe a little magic.) Mike is a man of few words, but when you get a chance to talk with him, it’s obvious that he’s smart and thoughtful and—when he wants to be—ridiculously funny. 
+</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="profile6" class="gridder-content">
+  <div class="row profile-inner profile-inner">
+    <h3>Autumn Beauchesne</h3>
+    <span>Game Developer and Programmer </span>
+    <div class="wrapper">
+      <div class="col-md-12 profile-expanded">
+        <p class="small">Autumn is a game developer and programmer extraordinaire. She brings expertise in both educational game design and the development of powerful, easy-to-use design tools. Autumn has been designing games ever since the mid 2000s, when she first began to express her ideas with GameMaker. She’s drawn to how games can be used to explore systems from mechanical and thematic angles. 
+</p>
+        <p class="small">One of Autumn’s coolest superpowers is her ability to bring other members of the Field Day team—including artists, writers, and other non-programmers—into the design process. She believes that the best games happen through collaboration, which means everyone needs to be able to contribute without being held back by technical constraints. Autumn’s game design tools remove bottlenecks and allow the whole team to create and iterate together. 
+</p>
+        <p class="small">Autumn cares about developing thoughtful games that make an impact. She was the sole developer and creative force behind Diffission (Filament Games), a fractions game that empowers kids to focus on self-improvement instead of perfection. She was also the sole developer for BeauRoutine, a tool used in nearly all of Filament’s Unity-based games, and the lead engineer for Do I Have a Right? (Filament Games), a civics game about constitutional law. Autumn has expertise in several design systems, but works most often in Unity and Typescript. 
+</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="profile7" class="gridder-content">
+  <div class="row profile-inner profile-inner">
+    <h3>Jennifer Scianna</h3>
+    <span>Research Intern and Director’s Minion </span>
+    <div class="wrapper">
+      <div class="col-md-12 profile-expanded">
+        <p class="small">Jenn’s research focuses on understanding student thinking through game data. Her work with Field Day began with data analytics for Lakeland and continues with a four-year project teaching science practices called Aqualab. She’s also a producer for Aqualab and game designer for Shipwrecks! 
+</p>
+        <p class="small">When it comes to advocating for kids, Jenn is unstoppable. As a former teacher, she brings a deep knowledge about how classrooms work and what teachers need. She works with the Teacher Fellows to design useful pedagogical tools, like a new teacher dashboard for Lakeland (soon to be applied across all our games). She takes the lead on user-testing with kids for many of our games. 
+</p>
+        <p class="small">In her role as Director’s Minion (or Assistant to the Director on paper), Jenn amazes everyone with her ability to keep all the administrative gears turning. Jenn’s superpowers include, but are not limited to: data analytics, ed tech, communicating with teachers, and keeping track of literally everything. 
+</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="profile8" class="gridder-content">
+  <div class="row profile-inner profile-inner">
+    <h3>Lindy Biller</h3>
+    <span>Writer</span>
+    <div class="wrapper">
+      <div class="col-md-12 profile-expanded">
+        <p class="small">Lindy is a creative writer, blogger, and copywriter. She brings a combination of keen story-building skills and intellectual depth to the worlds of both our games and our blog. Her years of experience working with teachers and learning game designers gives her a unique capacity as a writer. She contributes not only a love of story, but also the ability to learn about the complexities of education theory and game design.  
+</p>
+        <p class="small">Lindy wrote the scripts and in-game dialog for Jo Wilder and the Capitol Case, Lost at the Forever Mine, STEMPorts, Lakeland, and many of The Yard Games. She works closely with Sarah to write and refine all of our web copy, iterating until we are able to explain complex projects in just a few lines. 
+</p>
+        <p class="small">Lindy is the reason the farmers in Lakeland have folksy wit and the Artificial Intelligence in Forever Mine is relatable and dimensional. She crafts stories that allow kids to embody the spirit of a scientist, historian, or detective, and she fosters a sense of wonder in the process. 
+</p>
       </div>
     </div>
   </div>
