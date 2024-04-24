54fc484a88ef67818eedc8aa08e9c6151e05a659
Eric <extrapulpdesign@gmail.com>
2024-04-24 13:30:28 -0500

    added blank bloom single app page

---
+++ play/bloom/index.php
@@ -0,0 +1,96 @@
+<?php $path = $_SERVER['DOCUMENT_ROOT']; $path .= "/includes/header.php";  include_once($path); ?>
+
+<title>Bloom: The Saga of the Poop Fertilizer Economy.</title>
+<meta name="description" content="In this game, you’ll create trade networks and enact policies that keep farmers in business, while stopping excess manure from entering local lakes and rivers." />
+</head>
+<body class="singleapp bloom">
+
+<?php $path = $_SERVER['DOCUMENT_ROOT']; $path .= "/includes/main-nav.php";  include_once($path); ?>
+
+<!-- Main Content -->
+<section class="app-hero">
+  <div class="hero-content">
+    <div class="info-panel">
+      <ul class="breadcrumbs">
+        <li><a href="/">Field Day</a></li>
+        <img class="breadcrumb-arrow" src="../../assets/img/ui/breadcrumb-arrow.png">
+        <li>Play</li>
+        <img class="breadcrumb-arrow" src="../../assets/img/ui/breadcrumb-arrow.png">
+        <li>Bloom</li>
+      </ul>
+    </div>
+    <div class="app-info">
+      <h1 class="app-title">Bloom</h1>
+    </div>
+    <div class="app-description"><p class="small">In this game, you’ll create trade networks and enact policies that keep farmers in business, while stopping excess manure from entering local lakes and rivers.</p></div>
+    <div class="buttons">
+      <a target="_blank" href="./game" class="button xsmall white filled">Play the Game</a>
+    </div>
+  </div>
+</section>
+
+<section id="app-about" class="app-about">
+
+  <div class="row container">
+    <div class="col-sm-4">
+      <div class="about-nav follow-scroll">
+        <ul>
+        <li><a href="#glance">At a Glance</a></li>
+        <li><a href="#gameplay">Gameplay</a></li>
+        <li><a href="#content">Content Areas</a></li>
+        <li><a href="#standards">Academic Standards</a></li>
+        <li><a href="#partners">Partners</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div id="glance" class="col-sm-8 about-rightpanel">
+      <h2 id="glance">At a Glance</h2>
+      <ul>
+        <li>Subject: Scientific Modeling </li>
+        <li>Grades: 5-9 </li>
+        <li>Playtime: 25-35 minutes </li>
+        <li>Play from a web browser—no logins or downloads required</li>
+        <li>Free to play </li>
+      </ul>
+      <h2 id="gameplay">Gameplay</h2>
+      <p>You are a material scientist who just crash-landed on an abandoned mining planet. You’re out of fuel, and your suit’s oxygen is running low. You need to mine enough fuel to power your ship, or you won’t survive.</p>
+      <p>You stumble into the old mining facility. That’s where you meet MAL, the mine’s Artificial Intelligence, who is thrilled to have someone to talk to. With MAL’s help, you’ll build scientific models to predict if you’re mining fast enough and what you need to do next. </p>
+      <p>Each level puts the student in the role of the scientific modeler. Students will set up equations, work with graphs, input data, and use models to make predictions. Along the way, as obstacles keep popping up, students will experience how models are used as a tool to make decisions.</p>
+      <p class="note">Note: For best compatibility, use Chrome</p>
+      <h2 id="content">Content Areas</h2>
+      <p>replace with content area text</p>
+      <h2 id="standards">Academic Standards</h2>
+
+                    <h3> Science: Crosscutting Concepts (CC)s</h3>
+                    <div class="row standards-block">
+                        <div class="col-sm-7">
+                            <p class="small"><span class="bold">Systems and System Models - Standard SCI.CC4</span><br>Students use science and engineering practices, disciplinary core ideas, and an understanding of systems and system models to make sense of phenomena and solve problems.</p>
+                            <p class="small"><span class="bold">Stability and Change - Standard SCI.CC7</span><br>Students use science and engineering practices, disciplinary core ideas, and an understanding of stability and change to make sense of phenomena and solve problems.</p>
+                        </div>
+                    </div>
+                    <h3>Science: Disciplinary Core Ideas (DCI)</h3>
+                    <div class="row standards-block">
+                        <div class="col-sm-7">
+                            <p class="small"><span class="bold">Earth and Space Science 3 (ESS3)</span><br>Earth and Human Activity - Standard SCI.ESS3: Students use science and engineering practices, crosscutting concepts, and an understanding of earth and human activity to make sense of phenomena and solve problems.</p>
+                        </div>
+                    </div>
+
+
+      <h2 id="partners">Partners</h2>
+      <ul>
+        <li>Material Research Science & Engineering Center</li>
+        <li>Wisconsin Department of Public Instruction</li>
+      </ul>
+      
+      <p>Lost at the Forever Mine is an open-source project licensed under the <a target="_blank" href="https://opensource.org/licenses/MIT">MIT license</a> and is available at <a target="_blank" href="https://github.com/fielddaylab/forevermine">https://github.com/fielddaylab/forevermine</a></p>
+    </div>
+  </div>
+</section>
+
+<?php $path = $_SERVER['DOCUMENT_ROOT']; $path .= "/includes/footer.php";  include_once($path); ?>
+
+<?php $path = $_SERVER['DOCUMENT_ROOT']; $path .= "/includes/modals.php";  include_once($path); ?>
+
+</body>
+</html>
