21899484a3988a1013edd9f96c794d4c69be8ca3
jwilliams73 <jwilliams73@wisc.edu>
2024-09-10 11:31:16 -0500

    Games explore complex topics section

--- redesign.php
+++ redesign.php
@@ -6,84 +6,6 @@
 <?php include 'includes/main-nav.php';?>
 <!-- start page template -->
 <div class="home-page">
-    <section id="carousel">
-        <div id="carousel-hero" class="carousel slide">
-
-            
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner" role="listbox">
-                <div class="item active" style="background: url('assets/img/home/carousel/bloom-poster.jpg');">
-                    <a href="play/bloom">
-                        <span class="scanlines"></span>
-                        <video preload="none" muted loop class="carousel-video">
-                            <source src="assets/video/home/carousel/bloom.mov" type="video/mp4">
-                        </video>
-                     </a>
-                </div>
-                <div class="item" style="background: url('assets/img/home/carousel/wake-poster.jpg');">
-                    <a href="play/wake">
-                        <span class="scanlines"></span>
-                        <video preload="none" muted loop class="carousel-video">
-                            <source src="assets/video/home/carousel/wake.mov" type="video/mp4">
-                        </video>
-                    </a>
-                </div>
-
-                <div class="item" style="background: url('assets/img/home/carousel/journalism-poster.jpg');">
-                    <a href="play/headlines">
-                        <span class="scanlines"></span>
-                        <video preload="none" muted loop class="carousel-video">
-                            <source src="assets/video/home/carousel/journalism.mov" type="video/mp4">
-                        </video>
-                    </a>
-                </div>
-                <div class="item item-nomobile" style="background: url('assets/img/home/carousel/shipwrecks-poster.jpg');">
-                    <a href="play/emerald">
-                        <span class="scanlines"></span>
-                        <video preload="none" muted loop class="carousel-video">
-                            <source src="assets/video/home/carousel/shipwrecks.mov" type="video/mp4">
-                        </video>
-                     </a>
-                </div>
-
-            </div>
-
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-hero" data-slide-to="0" class="active">
-                    <img src="assets/img/games/thumbs/bloom.jpg" alt="Bloom Thumbnail" />
-                    <div class="thumb-caption">Bloom: The Saga of the Fertilizer Economy</div>
-                </li>
-                <li data-target="#carousel-hero" data-slide-to="1">
-                    <img src="assets/img/games/thumbs/wake.jpg" alt="Wake Thumbnail" />
-                    <div class="thumb-caption">Wake: Tales From The Aqualab</div>
-                </li>
-                <li data-target="#carousel-hero" data-slide-to="2">
-                    <img src="assets/img/games/thumbs/headlines-thumb.jpg" alt="Headlines Thumbnail" />
-                    <div class="thumb-caption">Headlines and High Water</div>
-                </li>
-                <li data-target="#carousel-hero" data-slide-to="3" class="indicator-nomobile">
-                    <img src="assets/img/games/thumbs/emerald.jpeg" alt="Emerald Thumbnail" />
-                    <div class="thumb-caption">Legend of the Lost Emerald</div>
-                </li>
-                <li>
-                    <a href="play">
-                        <div class="chevron"></div>
-                        <div class="thumb-caption">
-                                Play all of our games
-                        </div>
-                    </a>
-                </li>
-            </ol>
-
-            <div class="info">
-                <img src="assets/img/logos/fd-logo-yellow.png" alt="Field Day - Learning Games" />
-                <p>We're artists and researchers who make beautiful, impactful educational video games.</p>
-                <a href="about" class="button yellow filled small">Learn More</a>
-            </div>
-
-        </div>
-    </section>
     <section id="welcome">
         <h2>Welcome To Field Day</h2>
         <div class="ship">
@@ -105,6 +27,19 @@
                 <h4>Games let you try on a new identity.</h4>
                 <p>Games are an important tool for teachers because they allow players to take on the role of scientists, journalists, explorers, and more.</p>
             </div>
+        </div>
+    </section>
+    <div class="bottom-frame"></div>
+    <section id="complextopics" class="lead d-flex flex-col flex-xxl-reverse">
+        <div class="workshop-container">
+            <div class="workshop">
+            </div>
+        </div>
+        <div class="container hero-text">
+            <h4>Games explore complex topics.</h4>
+            <p>It's hard to communicate super complex topics in a short time frame. Games turn complicated topics into fun,
+                hands-on experiences that actually reach people. Our games get millions of plays.
+            </p>
         </div>
     </section>
     <div class="bottom-frame"></div>
