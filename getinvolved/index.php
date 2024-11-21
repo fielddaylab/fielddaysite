cbd8f6596608235cb559bc119949fb860c347046
XanGrab <60899653+XanGrab@users.noreply.github.com>
2024-11-21 17:25:21 -0600

    hero finished on get involved page

---
+++ getinvolved/index.php
@@ -0,0 +1,42 @@
+<!--include head -->
+<?php $path = $_SERVER['DOCUMENT_ROOT']; $path .= "/includes/header.php";  include_once($path); ?>
+<title>Get Involved - Field Day</title>
+<meta name="description" content="Get Involved - Field Day">
+</head>
+
+<body class="nav-fixed get-involved link-getinvolved page">
+<!--include Main Navigation
+------------------------>
+<?php $path = $_SERVER['DOCUMENT_ROOT']; $path .= "/includes/main-nav.php";  include_once($path); ?>
+
+  <div id="mainwrapper">
+  <!--Start page content -->
+
+    <div class="get-involved">
+        <!-- Hero Section -->
+        <section class="singlehero page_hero">
+            <div class="container">
+                <h1>Get involved with Field Day</h1>
+            </div>
+          <div class="helm-frame"></div>
+        </section> 
+
+        <!-- Marquee Section -->
+        <section id="marquee">
+          <div class="marquee__inner" aria-hidden="true">
+            <span>WE ARE PERISHING FOR LACK OF WONDER, NOT FOR LACK OF WONDERS.</span>
+            <!-- <span>WE ARE PERISHING FOR LACK OF WONDER, NOT FOR LACK OF WONDERS.</span>
+            <span>WE ARE PERISHING FOR LACK OF WONDER, NOT FOR LACK OF WONDERS.</span> -->
+          </div>
+        </section>
+
+    </div>
+
+  <!-- End page content -->
+  </div>
+
+<!--include footer -->
+<?php $path = $_SERVER['DOCUMENT_ROOT']; $path .= "/includes/footer.php";  include_once($path); ?>
+
+</body>
+</html>
