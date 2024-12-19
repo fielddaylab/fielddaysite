d0e6a2af81fbb05c3280704a7f37de9acc5cf6eb
XanGrab <60899653+XanGrab@users.noreply.github.com>
2024-12-19 12:38:44 -0600

    added vault button to atomtouch

--- play/atom-touch/index.php
+++ play/atom-touch/index.php
@@ -7,7 +7,7 @@ include_once($path); ?>
   function playContent() {
     document.getElementById("content_play").innerHTML = "";
     document.getElementById("content_play").style.display = "none";
-    document.getElementById("content").src = "build/iframe.html";
+    document.getElementById("content").src = "atom-touch/build/iframe.html";
     document.getElementById("content").style.display = "block";
   }
 </script>
@@ -50,7 +50,7 @@ include_once($path); ?>
   <section id="app-intro">
     <h2>Play Atom Touch</h2>
     <div id="content_play" style="width:880px; height:660px;" onclick="playContent()"><span class="button white filled">Play Atom Touch</span></div>
-    <iframe id="content" scrolling="no" style="width:880px; height:660px; display:none; margin:10px auto; overflow:hidden; border:0px;" src="build/null.html"></iframe>
+    <iframe id="content" scrolling="no" style="width:1280px; height:800px; display:none; margin:10px auto; overflow:hidden; border:0px;" src="atom-touch/build/null.html"></iframe>
     <h2>You can also download atom touch: </h2>
     <a href="https://itunes.apple.com/us/app/atomtouch/id1022112547?mt=8" target="_blank"><img class="play-button" src="/assets/img/buttons/apple-badge.png"></a>
     <a href="https://play.google.com/store/apps/details?id=edu.wisc.wid.fielddaylab.atomtouch&hl=en" target="_blank"><img class="play-button" src="/assets/img/buttons/google-play-badge.png"></a>
