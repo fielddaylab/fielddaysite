1b2f44517c91e55299ea371887f20b976bdb277b
Reyna <reynagroff@gmail.com>
2025-09-12 15:00:09 -0500

    Create Project Hercules Single App Page
    created a new page for Project Hercules. (Content not updated.)

---
+++ play/project-hercules/index.php
@@ -0,0 +1,156 @@
+<?php $path = $_SERVER['DOCUMENT_ROOT']; $path .= "/includes/header.php";  include_once($path); ?>
+
+<title>Project Hercules</title>
+<meta name="description" content="Project Hercules is a game about astronomy, and love, and distance, and the apocalypse, and parental relationships, and isolation, and mythology, and letters, and computers, and probably a bunch of other things I’m forgetting." />
+
+<script>
+window.onload = function()
+{
+  function detectMobile() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+}
+  console.log("Mobile Device: " + detectMobile());
+  if (detectMobile()) {
+    button = document.getElementById("playButton");
+    button.target="_self";
+    button.href = "javascript:void(0)";
+    button.classList.remove("filled");
+    button.textContent = "Play on Desktop";
+    console.log("In the if");
+  }
+};
+</script>
+
+
+</head>
+<body class="singleapp bloom">
+
+<?php $path = $_SERVER['DOCUMENT_ROOT']; $path .= "/includes/main-nav.php";  include_once($path); ?>
+
+<!-- Main Content -->
+<section class="app-hero">
+  <div class="hero-content">
+    <div class="info-panel">
+      <ul class="breadcrumbs">
+        <li><a href="/">Field Day</a></li>
+        <img class="breadcrumb-arrow" src="../../assets/img/ui/breadcrumb-arrow.png">
+        <li>Play</li>
+        <img class="breadcrumb-arrow" src="../../assets/img/ui/breadcrumb-arrow.png">
+        <li>Project Hercules</li>
+      </ul>
+    </div>
+    <div class="app-info">
+      <h1 class="app-title">Project Hercules</h1>
+    </div>
+    <div class="app-description"><p class="small">Project Hercules is a game about astronomy, and love, and distance, and the apocalypse, and parental relationships, and isolation, and mythology, and letters, and computers, and probably a bunch of other things I’m forgetting.</p></div>
+    <div class="buttons">
+              <a id = "playButton" class="button small white filled" href="https://fielddaylab.wisc.edu/play/project-hercules/ci/production/" target="_blank">Play Project Hercules</a>
+
+    </div>
+  </div>
+</section>
+
+<section id="app-about" class="app-about">
+
+  <div class="row container">
+    <div class="col-sm-4">
+      <div class="about-nav follow-scroll">
+        <ul>
+        <li><a href="#glance">At a Glance</a></li>
+        <li><a href="#gameplay">Gameplay</a></li>
+        <li><a href="#content">Content Areas</a></li>
+        <li><a href="#standards">Academic Standards</a></li>
+        <li><a href="#partners">Partners</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div id="glance" class="col-sm-8 about-rightpanel">
+      <div id="app-video" class="app-video">
+          <div class='embed-container'>
+              <iframe src="https://player.vimeo.com/video/1018790859" width="100%" frameborder="0" allowfullscreen></iframe>
+          </div>
+      </div>
+      <h2 id="glance">At a Glance</h2>
+      <ul>
+        <li>Grades: 9-12 </li>
+        <li>Playtime: Most players can complete the game in two 45-minute class periods. Players can save their progress, making it usable within a typical classroom context.  </li>
+      </ul>
+
+      <h2>About the Game</h2>
+
+      <p>In Bloom: The Saga of the <s>Poop</s> Fertilizer Economy, players serve as the director of SAGA:The Sustainable Agricultural Growth Administration. In this role, their goal is to foster economic growth across five different counties while reducing agricultural runoff and gross algal blooms. To win, players must develop local trade networks, employ a range of economic and environmental policies, and balance the needs and perspectives of bickering townsfolk. A key focus of the game is learning about the relationship between agricultural runoff and water quality, while exploring how markets that allow farmers to sell excess manure can lead to cleaner water and economic sustainability. 
+</p>
+      <h2 id="gameplay">Gameplay</h2>
+      <p>As the director of SAGA, players can use tax revenues to build roads (and other infrastructure) and enact economic and environmental policies. Their objective is to create local trade networks and utilize new technologies to develop a fertilizer market, whereby excess manure can be bought and sold, rather than inefficiently spread on fields. </p>
+      <p>Players start by overseeing a single county. As the game unfolds, additional counties are unlocked. In addition to managing the economy within each county, to be successful players must develop strategies for developing trade between counties. 
+ </p>
+      <p>Players' progress is saved and a player code, which is assigned at the start of the game, can be used to return at a later point. Note that when players return to a game they start at the beginning of the most recently unlocked county. Therefore, it is recommended that if players plan on returning to a saved game that they stop playing after a new county is unlocked, so that they don’t lose any progress they have made. 
+</p>
+      <h2 id="content">Content Areas</h2>
+      <ul>
+        <li>Environmental Science</li>
+        <li>Agricultural Science</li>
+        <li>Biology</li>
+        <li>Economics </li>
+        <li>Geography </li>
+        <li>Government (Environmental and Economic Policies) </li>
+      </ul>
+      <h2 id="standards">Academic Standards</h2>
+
+                    <h3> Science: Crosscutting Concepts (CC)</h3>
+                    <div class="row standards-block">
+                        <div class="col-sm-7">
+                            <p class="small"><span class="bold">Systems and System Models - Standard SCI.CC4</span><br>Students use science and engineering practices, disciplinary core ideas, and an understanding of systems and system models to make sense of phenomena and solve problems.</p>
+                            <p class="small"><span class="bold">Stability and Change - Standard SCI.CC7</span><br>Students use science and engineering practices, disciplinary core ideas, and an understanding of stability and change to make sense of phenomena and solve problems.</p>
+                        </div>
+                    </div>
+                    <h3>Science: Disciplinary Core Ideas (DCI)</h3>
+                    <div class="row standards-block">
+                        <div class="col-sm-7">
+                            <p class="small"><span class="bold">Earth and Space Science 3 (ESS3) — Earth and Human Activity - Standard SCI.ESS3:</span><br> Students use science and engineering practices, crosscutting concepts, and an understanding of earth and human activity to make sense of phenomena and solve problems.</p>
+                        </div>
+                    </div>
+                    <h3>Science: Science and Engineering Practices (SEP)</h3>
+                    <div class="row standards-block">
+                        <div class="col-sm-7">
+                            <p class="small"><span class="bold">Asking Questions and Defining Problems - Standard SCI.SEP1: </span><br> Students ask questions and define problems, in conjunction with using crosscutting concepts and disciplinary core ideas, to make sense of phenomena and solve problems.</p>
+                            <p class="small"><span class="bold">Analyze and Interpret Data - Standard SCI.SEP4:</span><br> Students analyze and interpret data, in conjunction with using crosscutting concepts and disciplinary core ideas, to make sense of phenomena and solve problems.</p>
+                            <p class="small"><span class="bold">Mathematics and Computational Thinking - Standard SCI.SEP5</span><br> Students use mathematics and computational thinking, in conjunction with using crosscutting concepts and disciplinary core ideas, to make sense of phenomena and solve problems.</p>
+                             <p class="small"><span class="bold">Construct Explanations and Design Solutions - Standard SCI.SEP6:</span><br>  Students construct explanations and design solutions, in conjunction with using crosscutting concepts and disciplinary core ideas, to make sense of phenomena and solve problems.</p>
+
+                        </div>
+                    </div>
+                    <h3>Social Studies</h3>
+                    <div class="row standards-block">
+                        <div class="col-sm-7">
+                            <p class="small"><span class="bold">Standard SS.Econ1:</span><br> Wisconsin students use economic reasoning to understand issues.
+                            <p class="small"><span class="bold">Standard SS.Econ2:</span><br> Wisconsin students will analyze how decisions are made and interactions occur among individuals, households, and firms or businesses (Microeconomics).</p>
+                            <p class="small"><span class="bold">Standard SS.Econ3:</span><br> Wisconsin students will analyze how an economy functions as a whole (Macroeconomics).</p>
+                            <p class="small"><span class="bold">Standard SS.Econ4:</span><br> Wisconsin students will evaluate government decisions and their impact on individuals, businesses, markets, and resources (Role of Government).</p>
+                            <p class="small"><span class="bold">Standard SS.Geog5:</span><br> Wisconsin students will evaluate the relationship between humans and the environment.</p></div>
+                    </div>
+
+      <h2 id="partners">Partners</h2>
+     <p>Bloom was designed with our amazing partners at UW-Madison’s Scalable Systems Lab and the experts at U.S. Environmental Protections Agency, including Gerardo  Ruiz-Mercado (EPA), David Cole (UW-Madison), and Lauren Knose (EPA). Special thanks to Victor Zavala and his students for sharing their time and expertise. Throughout the design and testing process, we relied on the experience and insights of Field Day teacher fellows and student play-testers from across Wisconsin.</p>
+    </div>
+  </div>
+</section>
+
+<?php $path = $_SERVER['DOCUMENT_ROOT']; $path .= "/includes/footer.php";  include_once($path); ?>
+
+<?php $path = $_SERVER['DOCUMENT_ROOT']; $path .= "/includes/modals.php";  include_once($path); ?>
+
+</body>
+</html>
